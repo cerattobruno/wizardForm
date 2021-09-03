9d2fcd820080155473da807fcc79e6883168526d
Bruno Ceratto <cerattobruno@gmail.com>
2021-09-03 18:52:10 -0300

    Maquetado de componentes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { InsuranceComponent } from './components/insurance/insurance.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent,  pathMatch: 'full' },
+  { path: 'asegurate', component: InsuranceComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
